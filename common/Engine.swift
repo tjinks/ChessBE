17ab6d8a41bd3c6e490bf09f8d4ff70aa5b93cc5
Tony Jinks <tony.jinks@gmail.com>
2024-06-20 10:52:41 +0100

    Ongoing dev

--- common/Engine.swift
+++ common/Engine.swift
@@ -12,7 +12,7 @@ typealias EngMovePtr = UnsafeMutablePointer<EngMove>
 typealias EngMoveListPtr = UnsafeMutablePointer<EngMoveList>
 typealias EngGamePtr = UnsafeMutablePointer<EngGame>
 
-class Position {
+public class Position {
     fileprivate let engPosition: EngPositionPtr
 
     init() {
@@ -35,9 +35,63 @@ class Position {
         }
     }
     
+    public subscript(index: Int) -> EngPiece {
+        get {
+            let ptr = engPosition.pointee.board! + index
+            return ptr.pointee
+        }
+        
+        set(value) {
+            let ptr = engPosition.pointee.board! + index
+            ptr.pointee = value
+        }
+    }
+    
+    public var playerToMove: EngPlayer {
+        get { return engPosition.pointee.playerToMove }
+        set(value) { engPosition.pointee.playerToMove = value }
+    }
+    
+    public var whiteCanCastleShort: Bool {
+        get { return engPosition.pointee.whiteCanCastleShort }
+        set(value) { engPosition.pointee.whiteCanCastleShort = value }
+    }
+    
+    public var whiteCanCastleLong: Bool {
+        get { return engPosition.pointee.whiteCanCastleLong }
+        set(value) { engPosition.pointee.whiteCanCastleLong = value }
+    }
+    
+    public var blackCanCastleShort: Bool {
+        get { return engPosition.pointee.blackCanCastleShort }
+        set(value) { engPosition.pointee.blackCanCastleShort = value }
+    }
+    
+    public var blackCanCastleLong: Bool {
+        get { return engPosition.pointee.blackCanCastleLong }
+        set(value) { engPosition.pointee.blackCanCastleLong = value }
+    }
+    
+    public var epSquare: Int {
+        get { return Int(engPosition.pointee.epSquare) }
+        set(value) {engPosition.pointee.epSquare = EngSquare(value) }
+    }
+
+    public var halfMoveClock: Int {
+        get { return Int(engPosition.pointee.halfMoveClock) }
+        set(value) { engPosition.pointee.halfMoveClock = EngMoveCounter(value) }
+    }
+    
+    public var moveNumber: Int {
+        get { return Int(engPosition.pointee.moveNumber) }
+        set(value) { engPosition.pointee.moveNumber = EngMoveCounter(value) }
+    }
+    
     deinit {
         engFreePosition(engPosition)
     }
+    
+    
 }
 
 class Game {
@@ -54,8 +108,66 @@ class Game {
         }
     }
     
+    func getTargets(from: Int) -> [Int] {
+        let resultMask = engGetTargets(game, EngSquare(from))
+        var bitMask = EngSquareMask(1)
+        var result = [Int]()
+        for i in 0..<64 {
+            if ((bitMask << i) & resultMask) != 0 {
+                result.append(i)
+            }
+        }
+        
+        return result
+    }
+    
+    func getMoves(from: Int, to: Int) -> MoveList {
+        let moveList = engGetMovesByFromAndTo(game, EngSquare(from), EngSquare(to))
+        return MoveList(moveList: moveList!)
+    }
+    
     deinit {
         engFreeGame(game)
     }
+}
+
+public class Move {
+    fileprivate let move: EngMovePtr
     
+    init(move: EngMovePtr) {
+        self.move = move
+    }
+    
+    public func makeMove() {
+        engMakeMove(move)
+    }
+}
+
+public class MoveList {
+    private var moves: [Move]
+    private let moveList: EngMoveListPtr
+    
+    fileprivate init(moveList: EngMoveListPtr) {
+        moves = []
+        self.moveList = moveList
+        var current = moveList.pointee.firstMove
+        while current != nil {
+            moves.append(Move(move: current!))
+            current = current!.pointee.nextMove
+        }
+    }
+    
+    public subscript(index: Int) -> Move {
+        get {
+            return moves[index]
+        }
+    }
+    
+    public var size: Int {
+        return moves.count
+    }
+    
+    deinit {
+        engFreeMoveList(moveList)
+    }
 }
